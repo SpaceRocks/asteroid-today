7ee1552ef0606ddb1a707613957688f2355b047b
David Greenfield <dgreenfield0@users.noreply.github.com>
2015-04-12 22:14:43 -0700

    Update with base setup.  Shows name and distance

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
 var UI = require('ui');
 var ajax = require('ajax');
 
-// Construct URL
-var URL = 'https://raw.githubusercontent.com/AsteroidTracker/AsteroidTrackerService/master/v2/neo_upcoming/upcoming';
+// var URL = 'https://raw.githubusercontent.com/AsteroidTracker/AsteroidTrackerService/master/v2/neo_upcoming/upcoming';
+var URL = 'https://raw.githubusercontent.com/AsteroidTracker/AsteroidTrackerService/master/v2/neo_recent/recent';
 var asteroids = [];
 
+
+function getMonthPlus(date) {
+    var month = date.getMonth()+1;
+    return month < 10 ? '0' + month : month; // ('' + month) for string result
+}  
+
 var d = new Date();
-var todayDate = d.getFullYear() + "-" + d.getMonth() + "-" + d.getDay();
+//var month = d.getMonth()+1;
+var todayDate = d.getFullYear() + "-" + getMonthPlus(d) + "-" + d.getDate();
 
 // Make the request
 ajax(
@@ -21,13 +28,14 @@ ajax(
       for(var i = 0; i < data.length; i++) {
         var obj = data[i];
         console.log("DATA 2" + obj.date_group );
+        console.log("DATe today" + todayDate );
         if (obj.date_group == todayDate) {
             for(var n = 0; n < obj.neo_entities.length; n++) {
                 console.log("DATA 2" + obj.neo_entities[n].name);                
                 asteroids.push( {
                         title: obj.neo_entities[n].name,
                         subtitle: "Lunar Distance " + obj.neo_entities[n].miss_distance_ld
-                    });
+                });
             }
         }
     }
