a5306fa0b85f99141e0c8d4e929b9ac37c2684bf
David Greenfield <dgreenfield0@users.noreply.github.com>
2015-04-28 01:51:26 -0700

    Updated service to test using neows

--- src/app.js
+++ src/app.js
@@ -1,10 +1,6 @@
 var UI = require('ui');
 var ajax = require('ajax');
-
-// var URL = 'https://raw.githubusercontent.com/AsteroidTracker/AsteroidTrackerService/master/v2/neo_upcoming/upcoming';
-var URL = 'https://raw.githubusercontent.com/AsteroidTracker/AsteroidTrackerService/master/v2/neo_recent/recent';
 var asteroids = [];
-
 
 function getMonthPlus(date) {
     var month = date.getMonth()+1;
@@ -12,45 +8,41 @@ function getMonthPlus(date) {
 }  
 
 var d = new Date();
-//var month = d.getMonth()+1;
 var todayDate = d.getFullYear() + "-" + getMonthPlus(d) + "-" + d.getDate();
 
-// Make the request
-ajax(
-  {
-    url: URL,
-    type: 'json'
-  },
-  function(data) {
-    // Success!
-    console.log("Successfully fetched Asteroid data!", data);
+// var URL = 'https://raw.githubusercontent.com/AsteroidTracker/AsteroidTrackerService/master/v2/neo_recent/recent';
+// var URL = 'https://raw.githubusercontent.com/AsteroidTracker/AsteroidTrackerService/master/v2/neo_upcoming/upcoming';
+// http://mysterious-thicket-9681.herokuapp.com/rest/v1/feed?start_date=2015-04-27&end_date=2015-04-27
+var URL = "http://mysterious-thicket-9681.herokuapp.com/rest/v1/feed?start_date="+todayDate+"&end_date="+todayDate;
 
-      for(var i = 0; i < data.length; i++) {
-        var obj = data[i];
-        console.log("DATA 2" + obj.date_group );
-        console.log("DATe today" + todayDate );
-        if (obj.date_group == todayDate) {
-            for(var n = 0; n < obj.neo_entities.length; n++) {
-                console.log("DATA 2" + obj.neo_entities[n].name);                
-                asteroids.push( {
-                        title: obj.neo_entities[n].name,
-                        subtitle: "Lunar Distance " + obj.neo_entities[n].miss_distance_ld
-                });
-            }
-        }
-    }
+     ajax({
+       url: URL,
+       type: 'json'
+     },
+     function(data) {
+     // Success!
+//     console.log("Successfully fetched Asteroid data!" + data);
+     var object = data;
+     var nearEarthObjects = object.near_earth_objects ;
+     for(var i = 0; i < nearEarthObjects[todayDate].length; i++) {
+         var obj = nearEarthObjects[todayDate][i];
+         var lunarDistance = obj.close_approach_data[0].miss_distiance.lunar_distance;
+         asteroids.push( {
+             title: obj.name,
+             subtitle: "Lunar Distance " + lunarDistance
+         });
+     }
 
-    // Create the Menu, supplying the list of fruits
-      var asteroid_list = new UI.Menu({
-        sections: [{
-          title: 'Asteroid Today',
-          items: asteroids
-        }]
-      });
-      asteroid_list.show();
+    var asteroid_list = new UI.Menu({
+      sections: [{
+        title: 'Asteroid Today',
+        items: asteroids
+       }]
+     });
+    asteroid_list.show();
   },
   function(error) {
     // Failure!
-    console.log('Failed fetching weather data: ' + error);
+    console.log('Failed fetching Asteroid data: ' + error);
   }
 );
